cc09241427b54662b24899bcd46723d06ed44840
cjalvaradoh <cjalvarado@gmail.com>
2024-09-28 13:34:41 -0600

    galeria

---
+++ galeriaFoto1002/Form1.Designer.vb
@@ -0,0 +1,60 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Button1 = New Button()
+        FlowLayoutPanel1 = New FlowLayoutPanel()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(176, 24)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(122, 23)
+        Button1.TabIndex = 0
+        Button1.Text = "Subir Imagen"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' FlowLayoutPanel1
+        ' 
+        FlowLayoutPanel1.Location = New Point(12, 65)
+        FlowLayoutPanel1.Name = "FlowLayoutPanel1"
+        FlowLayoutPanel1.Size = New Size(460, 284)
+        FlowLayoutPanel1.TabIndex = 1
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(484, 361)
+        Controls.Add(FlowLayoutPanel1)
+        Controls.Add(Button1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents FlowLayoutPanel1 As FlowLayoutPanel
+
+End Class
---
+++ galeriaFoto1002/Form1.vb
@@ -0,0 +1,20 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim openFileDialog As New OpenFileDialog() 'presentar dialogo al usuario'
+        openFileDialog.Multiselect = True 'habilita cargar multiples fotos'
+        openFileDialog.Filter = "Archivos de imagen |*.jpg;*.jpeg;*.png;*.bmp"
+
+        If openFileDialog.ShowDialog() = DialogResult.OK Then
+            'pueden ser varias imagenes, por ello se ejecuta un ciclo'
+            For Each archivo As String In openFileDialog.FileNames
+                Dim miniatura As New PictureBox() 'Se crea nueva caja de imagen'
+                miniatura.SizeMode = PictureBoxSizeMode.Zoom
+                miniatura.Image = Image.FromFile(archivo)
+                miniatura.Width = 100
+                miniatura.Height = 100
+
+                FlowLayoutPanel1.Controls.Add(miniatura)
+            Next
+        End If
+    End Sub
+End Class
